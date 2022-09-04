c6b93d8a98db68127120a116e9791e39134a3dae
Razib2019 <hafizurrahmanrazib@gmail.com>
2022-09-04 09:28:36 +0600

    get id from news

--- js/newsCategories.js
+++ js/newsCategories.js
@@ -7,7 +7,6 @@ const loadCategories = () => {
 const displayCategories = categories => {
     const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(category => {
-        // console.log(category);
         const categoryLi = document.createElement('li');
         categoryLi.classList.add('nav-item');
         categoryLi.innerHTML = `
@@ -28,7 +27,7 @@ const displayCategoryDetails = categories => {
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = '';
     categories.forEach(category => {
-        console.log(category);
+        // console.log(category);
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
         <div class="card mb-5">
@@ -36,33 +35,32 @@ const displayCategoryDetails = categories => {
                     <div class="col-md-5">
                         <img src="${category.image_url}" class="img-fluid rounded-start" alt="...">
                     </div>
-                    <div class="col-md-7 d-flex align-items-center ">
+                    <div class="col-md-7">
                         <div class="card-body">
                             <h4 class="card-title fw-bold">${category.title}</h4>
                             <p class="card-text">${category.details.slice(0, 250)}...</p>
                             <div class="row g-2">
                                 <div class="col-lg-4 col-md-6 col-sm-6">
                                     <div class="p-3 ">
-                                        <div class=" d-flex">
+                                        <div class="d-flex align-items-center">
                                             <img style="height: 50px;width: 50px;" class="img-fluid" src="${category.author.img}" alt="">
                                             <div class="ps-3">
                                                 <h6>${category.author.name}</h4>
-                                                    <p>${category.author.published_date}</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-lg-4 col-md-6 col-sm-6">
                                     <div class="p-3">
-                                        <div class=" text-center">
+                                        <div>
                                             <p><span><i class="fa-regular fa-eye pe-2"></i></span>${category.total_view}</p>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-lg-4 col-md-6 col-sm-6">
                                     <div class="p-3">
-                                        <div class=" text-center">
-                                            <span><i class="fa-solid fa-arrow-right"></i></span>
+                                        <div>
+                                            <a onclick="loadNewsDetails('${category._id}')" href="#"><span><i class="fa-solid fa-arrow-right"></i></span></a>
                                         </div>
                                     </div>
                                 </div>
@@ -74,6 +72,18 @@ const displayCategoryDetails = categories => {
         `;
         newsContainer.appendChild(newsDiv);
     })
+}
+
+const loadNewsDetails = (newsId) => {
+    const url = `https://openapi.programming-hero.com/api/news/${newsId}`
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsDetails(data.data[0]))
+}
+
+const displayNewsDetails = news => {
+
 }
 
 loadCategories();
